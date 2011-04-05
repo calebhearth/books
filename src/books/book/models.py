52368ba1c19c28bbceee75c2c4e8872c0af7b538
cjaysson <devnull@localhost>
2011-04-05 16:46:22 -0700

    class members completed and admin page enabled

--- src/books/book/models.py
+++ src/books/book/models.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import User
 class Author(models.Model):
     authorName = models.CharField(max_length=200)
     
-class Tag(models.Model):
-    description = models.CharField(50)
     
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -19,3 +17,32 @@ class Book(models.Model):
     gutenbergURL = models.CharField(max_length=500)
     googleBooksURL = models.CharField(max_length=500)
     abeBooksURL = models.CharField(max_length=500)
+    
+class ISBN(models.Model):
+    isbn = models.CharField(max_length=16)
+    book = models.ForeignKey(Book)
+    
+class Tag(models.Model):
+    description = models.CharField(50)
+    book = models.ForeignKey(Book)
+    
+class Genre(models.Model):
+    description = models.CharField(50)
+    book = models.ForeignKey(Book)
+    
+class Suggestion(models.Model):
+    userFrom = models.ForeignKey(User, related_name="userFrom")
+    userTo = models.ForeignKey(User, related_name="userTo")
+    book = models.ForeignKey(Book)
+    message = models.CharField(max_length=1000)
+    
+class Review(models.Model):
+    user = models.ForeignKey(User)
+    book = models.ForeignKey(Book)
+    review = models.CharField(max_length=1000)
+    
+class Rating(models.Model):
+    user = models.ForeignKey(User)
+    book = models.ForeignKey(Book)
+    rating = models.IntegerField()
+    
--- src/books/urls.py
+++ src/books/urls.py
@@ -6,11 +6,11 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     # Example:
-    # (r'^books/', include('books.foo.urls')),
+    (r'^books/', include('books.book.views')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
