39e83eab6fb93e7e64e17823490b847badf6639f
cjaysson <devnull@localhost>
2011-04-06 01:31:41 -0700

    tag and genre matching for this_format_for_slugs

--- src/books/urls.py
+++ src/books/urls.py
@@ -6,4 +6,7 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^$', 'book.views.index'),
     (r'^book/(?P<book_id>\d+)/$', 'book.views.book'),
+    (r'^tag/(?P<tag_slug>[\w_]+\w)/$', 'book.views.tag'),
+    (r'^genre/(?P<genre_slug>\[\w_]+\w))/$', 'book.views.genre'),
+    
 )
