54ebb74530ece22af49e627dbf5b3fe0485789a3
cjaysson <devnull@localhost>
2011-04-05 22:06:39 -0700

    Book view

--- src/books/book/views.py
+++ src/books/book/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from books.book.models import Book
+from books.book.models import Book, Tag, Genre
 
 def index(request):
     return render_to_response('index.html')
@@ -9,8 +9,13 @@ def index(request):
 def book(request, book_id):
     try:
         b = Book.objects.get(pk=book_id)
+        tags = Tag.objects.get(book=b)
+        genres = Genre.objects.get(book=b)
+        
     except (Book.DoesNotExist):
         raise Http404
     return render_to_response('book.html', {
                                 'book' : b,
+                                'tags' : tags,
+                                'genres' : genres,
                             })
